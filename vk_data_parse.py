5c2586db0eb05cbe7b55942afc631e0764203692
bogdan <spawnvpn@gmail.com>
2017-01-13 17:28:56 +0200

    Refactored.

--- vk_data_parse.py
+++ vk_data_parse.py
@@ -1,10 +1,11 @@
 import inspect
 import random
-from fake_useragent import UserAgent
 import requests
 import urllib
-
-PROXIES = ['36.81.184.111:80', '70.248.28.23:800', '36.81.185.56:80', '104.199.117.203:80', '36.81.185.136:80', '104.196.233.61:80', '139.59.44.124:80', '97.77.104.22:3128', '122.129.74.147:8080', '103.52.134.97:8080', '110.164.58.147:9001', '36.81.184.192:80', '36.81.215.176:80', '191.252.100.77:8080', '63.128.95.200:8888', '36.81.185.29:80', '196.196.2.12:1080', '45.76.130.31:3128', '212.220.10.243:8080', '196.196.2.4:1080', '196.196.2.21:1080', '196.196.2.8:1080', '36.81.185.1:80', '35.185.8.144:80', '104.199.121.181:80', '36.81.184.129:80', '213.136.89.121:80', '213.136.77.246:80', '36.84.12.163:80', '36.81.184.23:80', '217.33.216.114:8080', '93.142.19.60:8080', '45.76.137.247:3128', '36.84.12.23:80', '36.84.12.142:80', '178.62.95.209:8118', '182.52.233.40:8080', '195.9.237.66:8080', '187.76.192.130:8080', '188.32.79.247:8081', '203.128.17.1:8080', '179.235.208.38:8080', '197.210.252.39:8080', '12.33.254.195:3128', '36.82.73.57:80', '37.72.185.34:1080', '47.90.74.111:8088', '37.72.185.2:1080', '37.72.185.3:1080', '178.161.149.18:3128', '47.89.41.164:80', '36.82.127.41:80', '36.81.184.119:80', '37.72.185.4:1080', '37.29.83.228:8080', '36.81.185.130:80', '93.175.29.101:2222', '188.170.211.206:8080', '36.81.184.171:80', '36.84.12.57:80', '95.31.3.34:8080', '36.81.184.46:80', '36.81.185.79:80', '23.24.89.194:7004', '36.81.185.233:80', '165.84.188.67:80', '36.84.12.212:80', '36.84.12.61:80', '36.84.12.47:80', '36.84.12.152:80', '36.84.12.217:80', '36.84.12.99:80', '36.84.12.196:80', '36.84.12.210:80', '36.84.12.33:80', '36.84.12.37:80', '36.81.185.3:80', '36.84.12.103:80', '37.72.185.43:1080', '37.29.83.212:8080']
+import pytumblr
+import vk
+from fake_useragent import UserAgent
+from requests.adapters import HTTPAdapter
 
 
 class RequestException(Exception):
@@ -18,127 +19,290 @@ class RequestException(Exception):
 class VkClient(object):
     host = 'https://api.vk.com/method/'
 
-    def __init__(self, token=None):
+    def __init__(self, token=None, proxy_list=None):
         self.token = token
+        self.proxy_list = proxy_list
         self.ua = UserAgent()
 
-    def _request(self, method, args):
-        args.update({'access_token': self.token}) if self.token else None
-        str_args = urllib.urlencode(args)
-        url = self.host + inspect.stack()[1][3] + '.' + method + '?' + str_args
-        data = requests.get(url, proxies={'http://': random.choice(PROXIES)}, headers={'User-Agent': self.ua.random}).json()
+    def _send(self):
+        pass
 
-        if data.get('error', ''):
-            raise RequestException(data.get('error').get('error_msg'))
+    def _request(self, api_method, api_action, args, request_method, allow_redirects=True, timeout=10, retries=5):
+        session = requests.Session()
+        session.mount('https://', HTTPAdapter(max_retries=retries))
+        session.mount('http://', HTTPAdapter(max_retries=retries))
+        session.headers.update({'User-Agent': self.ua.random})
+        session.proxies.update({'http://': random.choice(self.proxy_list)})
 
-        return data
+        if self.token:
+            args.update({'access_token': self.token})
 
-    def account(self, method, **kwargs):
-        return self._request(method, kwargs)
+        method = getattr(session, request_method)
+        try:
+            rq_kwargs = {}
+            if request_method == 'post':
+                rq_kwargs['data'] = args
+            elif request_method == 'get':
+                rq_kwargs['params'] = args
 
-    def apps(self, method, **kwargs):
-        return self._request(method, kwargs)
+            data = method(
+                url=self.host + api_method + '.' + api_action,
+                allow_redirects=allow_redirects,
+                timeout=timeout,
+                **rq_kwargs
+            )
 
-    def audio(self, method, **kwargs):
-        return self._request(method, kwargs)
+            if not data.ok:
+                raise RequestException('Received no 200 status code: ' + str(data.status_code))
 
-    def auth(self, method, **kwargs):
-        return self._request(method, kwargs)
+            if 'error' in data.text and 'error_msg' in data.text:
+                raise RequestException(data.json()['error']['error_msg'])
 
-    def board(self, method, **kwargs):
-        return self._request(method, kwargs)
+        except requests.exceptions.RetryError:
+            print 'Api resets the connection'
+        except requests.exceptions.Timeout:
+            print 'Api do not response'
+        except requests.exceptions.RequestException as e:
+            print e
 
-    def database(self, method, **kwargs):
-        return self._request(method, kwargs)
+        return data.json()
 
-    def docs(self, method, **kwargs):
-        return self._request(method, kwargs)
+    VALID_METHODS = (
+        'account',
+        'apps',
+        'audio',
+        'auth',
+        'board',
+        'database',
+        'docs',
+        'other',
+        'fave',
+        'friends',
+        'gifts',
+        'groups',
+        'likes',
+        'market',
+        'messages',
+        'newsfeed',
+        'notes',
+        'notifications',
+        'pages',
+        'photos',
+        'places',
+        'polls',
+        'search',
+        'stats',
+        'status',
+        'storage',
+        'users',
+        'utils',
+        'video',
+        'wall',
+        'widgets',
+    )
 
-    def other(self, method, **kwargs):
-        return self._request(method, kwargs)
+    def __getattr__(self, item):
+        if item not in self.VALID_METHODS:
+            raise AttributeError()
 
-    def fave(self, method, **kwargs):
-        return self._request(method, kwargs)
+        def execute(*args, **kwargs):
+            return self._request(api_method=item,
+                                 *args, **kwargs)
 
-    def friends(self, method, **kwargs):
-        return self._request(method, kwargs)
+        return execute
 
-    def gifts(self, method, **kwargs):
-        return self._request(method, kwargs)
+    # def account(self, api_method, **kwargs):
+    #     return self._request(api_method, kwargs)
+    #
+    # def apps(self, api_method, **kwargs):
+    #     return self._request(api_method, kwargs)
+    #
+    # def audio(self, api_method, request_method, **kwargs):
+    #     return self._request(api_method=api_method, request_method=request_method, args=kwargs)
+    #
+    # def auth(self, api_method, **kwargs):
+    #     return self._request(api_method, kwargs)
+    #
+    # def board(self, api_method, **kwargs):
+    #     return self._request(api_method, kwargs)
+    #
+    # def database(self, api_method, **kwargs):
+    #     return self._request(api_method, kwargs)
+    #
+    # def docs(self, api_method, **kwargs):
+    #     return self._request(api_method, kwargs)
+    #
+    # def other(self, api_method, **kwargs):
+    #     return self._request(api_method, kwargs)
+    #
+    # def fave(self, api_method, **kwargs):
+    #     return self._request(api_method, kwargs)
+    #
+    # def friends(self, api_method, **kwargs):
+    #     return self._request(api_method, kwargs)
+    #
+    # def gifts(self, api_method, **kwargs):
+    #     return self._request(api_method, kwargs)
+    #
+    # def groups(self, api_method, **kwargs):
+    #     return self._request(api_method, kwargs)
+    #
+    # def likes(self, api_method, **kwargs):
+    #     return self._request(api_method, kwargs)
+    #
+    # def market(self, api_method, **kwargs):
+    #     return self._request(api_method, kwargs)
+    #
+    # def messages(self, api_method, **kwargs):
+    #     return self._request(api_method, kwargs)
+    #
+    # def newsfeed(self, api_method, request_method, **kwargs):
+    #     return self._request(api_method=api_method, request_method=request_method, args=kwargs)
+    #
+    # def notes(self, api_method, **kwargs):
+    #     return self._request(api_method, kwargs)
+    #
+    # def notifications(self, api_method, **kwargs):
+    #     return self._request(api_method, kwargs)
+    #
+    # def pages(self, api_method, **kwargs):
+    #     return self._request(api_method, kwargs)
+    #
+    # def photos(self, api_method, **kwargs):
+    #     return self._request(api_method, kwargs)
+    #
+    # def places(self, api_method, **kwargs):
+    #     return self._request(api_method, kwargs)
+    #
+    # def polls(self, api_method, **kwargs):
+    #     return self._request(api_method, kwargs)
+    #
+    # def search(self, api_method, **kwargs):
+    #     return self._request(api_method, kwargs)
+    #
+    # def stats(self, api_method, **kwargs):
+    #     return self._request(api_method, kwargs)
+    #
+    # def status(self, api_method, **kwargs):
+    #     return self._request(api_method, kwargs)
+    #
+    # def storage(self, api_method, **kwargs):
+    #     return self._request(api_method, kwargs)
+    #
+    # def users(self, api_method, request_method, **kwargs):
+    #     return self._request(api_method=api_method, request_method=request_method, args=kwargs)
+    #
+    # def utils(self, api_method, **kwargs):
+    #     return self._request(api_method, kwargs)
+    #
+    # def video(self, api_method, **kwargs):
+    #     return self._request(api_method, kwargs)
+    #
+    # def wall(self, api_method, request_method, **kwargs):
+    #     return self._request(api_method=api_method, request_method=request_method, args=kwargs)
+    #
+    # def widgets(self, api_method, **kwargs):
+    #     return self._request(api_method, kwargs)
 
-    def groups(self, method, **kwargs):
-        return self._request(method, kwargs)
 
-    def likes(self, method, **kwargs):
-        return self._request(method, kwargs)
+proxy_list = ['36.81.184.111:80',
+              '70.248.28.23:800',
+              '36.81.185.56:80',
+              '104.199.117.203:80',
+              '36.81.185.136:80',
+              '104.196.233.61:80',
+              '139.59.44.124:80',
+              '97.77.104.22:3128',
+              '122.129.74.147:8080',
+              '103.52.134.97:8080',
+              '110.164.58.147:9001',
+              '36.81.184.192:80',
+              '36.81.215.176:80',
+              '191.252.100.77:8080',
+              '63.128.95.200:8888',
+              '36.81.185.29:80',
+              '196.196.2.12:1080',
+              '45.76.130.31:3128',
+              '212.220.10.243:8080',
+              '196.196.2.4:1080',
+              '196.196.2.21:1080',
+              '196.196.2.8:1080',
+              '36.81.185.1:80',
+              '35.185.8.144:80',
+              '104.199.121.181:80',
+              '36.81.184.129:80',
+              '213.136.89.121:80',
+              '213.136.77.246:80',
+              '36.84.12.163:80',
+              '36.81.184.23:80',
+              '217.33.216.114:8080',
+              '93.142.19.60:8080',
+              '45.76.137.247:3128',
+              '36.84.12.23:80',
+              '36.84.12.142:80',
+              '178.62.95.209:8118',
+              '182.52.233.40:8080',
+              '195.9.237.66:8080',
+              '187.76.192.130:8080',
+              '188.32.79.247:8081',
+              '203.128.17.1:8080',
+              '179.235.208.38:8080',
+              '197.210.252.39:8080',
+              '12.33.254.195:3128',
+              '36.82.73.57:80',
+              '37.72.185.34:1080',
+              '47.90.74.111:8088',
+              '37.72.185.2:1080',
+              '37.72.185.3:1080',
+              '178.161.149.18:3128',
+              '47.89.41.164:80',
+              '36.82.127.41:80',
+              '36.81.184.119:80',
+              '37.72.185.4:1080',
+              '37.29.83.228:8080',
+              '36.81.185.130:80',
+              '93.175.29.101:2222',
+              '188.170.211.206:8080',
+              '36.81.184.171:80',
+              '36.84.12.57:80',
+              '95.31.3.34:8080',
+              '36.81.184.46:80',
+              '36.81.185.79:80',
+              '23.24.89.194:7004',
+              '36.81.185.233:80',
+              '165.84.188.67:80',
+              '36.84.12.212:80',
+              '36.84.12.61:80',
+              '36.84.12.47:80',
+              '36.84.12.152:80',
+              '36.84.12.217:80',
+              '36.84.12.99:80',
+              '36.84.12.196:80',
+              '36.84.12.210:80',
+              '36.84.12.33:80',
+              '36.84.12.37:80',
+              '36.81.185.3:80',
+              '36.84.12.103:80',
+              '37.72.185.43:1080',
+              '37.29.83.212:8080']
 
-    def market(self, method, **kwargs):
-        return self._request(method, kwargs)
-
-    def messages(self, method, **kwargs):
-        return self._request(method, kwargs)
-
-    def newsfeed(self, method, **kwargs):
-        return self._request(method, kwargs)
-
-    def notes(self, method, **kwargs):
-        return self._request(method, kwargs)
-
-    def notifications(self, method, **kwargs):
-        return self._request(method, kwargs)
-
-    def pages(self, method, **kwargs):
-        return self._request(method, kwargs)
-
-    def photos(self, method, **kwargs):
-        return self._request(method, kwargs)
-
-    def places(self, method, **kwargs):
-        return self._request(method, kwargs)
-
-    def polls(self, method, **kwargs):
-        return self._request(method, kwargs)
-
-    def search(self, method, **kwargs):
-        return self._request(method, kwargs)
-
-    def stats(self, method, **kwargs):
-        return self._request(method, kwargs)
-
-    def status(self, method, **kwargs):
-        return self._request(method, kwargs)
-
-    def storage(self, method, **kwargs):
-        return self._request(method, kwargs)
-
-    def users(self, method, **kwargs):
-        return self._request(method, kwargs)
-
-    def utils(self, method, **kwargs):
-        return self._request(method, kwargs)
-
-    def video(self, method, **kwargs):
-        return self._request(method, kwargs)
-
-    def wall(self, method, **kwargs):
-        return self._request(method, kwargs)
-
-    def widgets(self, method, **kwargs):
-        return self._request(method, kwargs)
+# session.mount(HTTPAdapter(max_retries=5))
+# client = VkClient(proxy_list=proxy_list, )
+client = VkClient(proxy_list=proxy_list,
+                  token='40d31b3c7c18ef58888d070e6ff28bf26efddedd2ca08d4db9295d685167de9638f387d15a9d1e42f64ed',
+                  )
 
 
-client = VkClient()
-# client = VkClient(token='40d31b3c7c18ef58888d070e6ff28bf26efddedd2ca08d4db9295d685167de9638f387d15a9d1e42f64ed')
-
-for i in xrange(100):
-    print client.users('get', user_ids='aqustics', fields='sex, city, bdate, counters')
+print client.newsfeed(request_method='post',
+                      api_action='search',
+                      args={'owner_id':1584512,
+                      'q':'cats',
+                      'start_time':1286668800,
+                      'count':100}).get('response')
 
 
 
-
-
-
-
+# http://vk.api.com/methods/newsfeed.search?param=sadfasdas&
 
 
 
