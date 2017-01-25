dd2ea41db63061e4a166023b30eded8a02342c76
bogdan <spawnvpn@gmail.com>
2017-01-25 17:23:46 +0200

    Added DataFormatting class.

--- data_parse.py
+++ data_parse.py
@@ -9,14 +9,16 @@ monkey.patch_all()
 
 class AsyncRequests(object):
 
-    def __init__(self):
+    def __init__(self, timeout):
         self.jobs_pull = []
+        self.timeout = timeout
 
     def add_job(self, job):
         self.jobs_pull.append(job)
 
     def join_jobs(self):
-        for result in gevent.joinall(self.jobs_pull, timeout=4):
+        timeout = len(self.jobs_pull) * self.timeout
+        for result in gevent.joinall(self.jobs_pull, timeout=timeout):
             yield result.get()
 
 
@@ -27,7 +29,7 @@ class VKData(object):
         self.client = None
 
     def _fetch_data(self, method, param_key):
-        async_requests = AsyncRequests()
+        async_requests = AsyncRequests(timeout=3)
         items = self.parameters[param_key].split(',')
         for item in items:
             self.parameters.update({param_key: item})
@@ -56,8 +58,9 @@ class VKData(object):
             'posts': '15723625_5801, 15723625_5800,15723625_5799,15723625_5798,15723625_5797,\
             15723625_5796,15723625_5795,15723625_5794,15723625_5793, 15723625_5792,15723625_5791,\
             15723625_5790,15723625_5789,15723625_5788,15723625_5787,15723625_5786,15723625_5784,\
-            15723625_5783,15723625_5782,15723625_5779',
+            15723625_5783,15723625_5782,43291122_33348,15723625_5806',
             'access_token': '',
+            'extended': '1',
         }
         self.client = VKClient(token=self.parameters['access_token'], proxy_list=self.proxy_list)
 
@@ -88,7 +91,86 @@ class VKData(object):
         return self._fetch_data(param_key='user_ids', method=self.client.users.get)
 
 
+class DataFormatting(object):
+
+    def __init__(self, data_list):
+        self.data_list = data_list
+        self.output_post = {}
+
+    def _get_post_type(self, item):
+
+        ATTACHMENT_TYPES = {
+            'photo': 1,
+            'video': 2,
+            'link': 3,
+        }
+
+        if 'attachments' in item:
+            attachment_type = item['attachments'][0]['type']
+            for attachment in item['attachments']:
+                if attachment_type != attachment['type']:
+                    return 4
+                return ATTACHMENT_TYPES[attachment_type]
+
+        elif 'text' in item:
+            return 0
+
+        return 'no data'
+
+    def _get_post_from_list(self):
+        for post in self.data_list:
+            yield post
+
+    def _get_original_post(self, post):
+        original_post = post['items']['copy_history']
+        return original_post
+
+    def _get_thumbnail(self, content):
+        max_size = 0
+        url = ''
+        for key, value in content.items():
+            if key.startswith('photo_'):
+                size = key.split('_')[1]
+                if size > max_size:
+                    max_size = size
+                    url = value
+        return url
+
+    def _get_attachments(self, item):
+        for attachment in item['attachments']:
+            attachment_type = attachment['type']
+            attachment_content = attachment[attachment_type]
+            url = 'https://vk.com/' + attachment_type + attachment_content['id'] + '_' + attachment_content['owner_id']
+            self.output_post[attachment_type].append({'url': url,
+                                                      'thumbnail': self._get_thumbnail(attachment_content)})
+
+    def forming_post_dict(self):
+        posts = self.data_list
+
+        for post in posts:
+            item = post['items'][0]
+            self._get_post_type(item)
+
+            if 'copy_history' in item:
+                self.output_post['original_post'] = self._get_original_post(post)  # todo formatting origin post under basic
+
+            self.output_post['post_type'] = self._get_post_type(item)
+            self.output_post['vk_ids'] = {'post_id': item['id'],
+                                          'author_id': item['owner_id']}
+
+            if self.output_post['post_type'] != 0 and self.output_post['post_type'] != 'no data':
+                self._get_attachments(item)
+
+
+
+    def forming_unique_authors(self):
+        pass
+
 a = VKData()
 res = a.update_posts()
+
+# data_formatting = DataFormatting(res)
+# data_formatting.forming_post_dict()
+
 for item in res:
     pprint(item)
